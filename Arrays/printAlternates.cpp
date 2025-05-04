bc32de414903691f05c7099d3e9463eda12361c2
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2025-05-04 14:24:57 +0530

    Print  every alternate element of the array

---
+++ Arrays/printAlternates.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int arr[6] = {10, 20, 30, 40, 50, 60};
+
+    for(int i = 0; i<6; i +=2){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
